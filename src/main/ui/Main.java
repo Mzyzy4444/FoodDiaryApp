b415c3bced1b2c9ed3e404a8645a3388dd0f8fe2
zma14 <zma14@students.cs.ubc.ca>
2023-03-20 20:43:18 -0700

    #created a new Jfram object

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,10 +1,13 @@
 package ui;
 
+import ui.Gui.Gui;
+
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            new Gui();
             new FoodDiaryApp();
         } catch (FileNotFoundException e) {
             System.out.println("Unable to run application: file not found");
